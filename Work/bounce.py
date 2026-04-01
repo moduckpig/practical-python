5cbd8997d7db3ddb459c689edf70c1bc7fed4dd0
moduckpig <1828760176@qq.com>
2026-04-01 13:50:35 +0800

    完成Exercise 1.5弹跳小球代码

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+count = 0
+origin_hight = 100
+while count<10:
+    count += 1
+    bound_hight = origin_hight * (3/5)
+    origin_hight = bound_hight
+    print(count,end=" ")
+    print(bound_hight)
+    print(round(bound_hight,3))
