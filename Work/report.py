e1ba90c2ab1edbb6c9cb641cde612b2476a35e8a
moduckpig <1828760176@qq.com>
2026-04-09 20:37:17 +0800

    完成练习report代码

--- Work/report.py
+++ Work/report.py
@@ -3,6 +3,7 @@
 # Exercise 2.4
 import csv
 def read_portfolio(filename):
+
     with  open(filename,'rt') as f:
         portfolio = []
         lines = csv.reader(f)
@@ -18,6 +19,7 @@ def read_portfolio(filename):
                 continue
     return portfolio
 def read_price(filename):
+
     with open(filename,'rt') as f:
         prices = {}
         lines = csv.reader(f)
@@ -31,37 +33,52 @@ def read_price(filename):
                 continue
     return prices
 def make_report(portfolio,price):
+
     report = []
     for line in portfolio:
         row = (line['name'],line['shares'],price[line['name']],price[line['name']]-line['price'])
         report.append(row)
     return report
-portfolio = read_portfolio('Data/portfolio.csv')
-prices = read_price('Data/prices.csv')
-report = make_report(portfolio,prices)
-headers = ('Name','Shares','Price','Change')
-#%这种表示方法只能用于元组上
-print('%10s %10s %10s %10s' % headers)
-print(('-'*10+' ')*4)
-for r in report:
-    print('%10s %10d %10.2f$ %10.2f' % r)
-total_profit = 0.0
-profit = 0.0
-for row in portfolio:
-    name = row['name']
-    shares = row['shares']
-    xprice = row['price']
-    price = prices[name]
-    profit = shares*(xprice-price)
-    print(f'name:{name},profit:{profit:.2f}')
-    total_profit += profit
-print('total_profit:',total_profit)
-total_cost = 0.0
-cost = 0.0
-for row in portfolio:
-    cost = row['shares']*row['price']
-    total_cost += cost;
-print(f'total_cost:{total_cost:.2f}')
+def print_report(reportdata):
+
+    header = ('Name', 'Shares','Price','Change')
+    print('%10s %10s %10s %10s' % header)
+    print(('-'*10+' ')*4)
+    for r in reportdata:
+        print('%10s %10d %10.2f$ %10.2f' % r)
+def portfolio_report(portfoliofilename,pricefilename):
+
+    portfolio = read_portfolio(portfoliofilename)
+    price = read_price(pricefilename)
+    report = make_report(portfolio,price)
+    print_report(report)
+
+# portfolio = read_portfolio('Data/portfolio.csv')
+# prices = read_price('Data/prices.csv')
+# report = make_report(portfolio,prices)
+# headers = ('Name','Shares','Price','Change')
+# #%这种表示方法只能用于元组上
+# print('%10s %10s %10s %10s' % headers)
+# print(('-'*10+' ')*4)
+# for r in report:
+#     print('%10s %10d %10.2f$ %10.2f' % r)
+# total_profit = 0.0
+# profit = 0.0
+# for row in portfolio:
+#     name = row['name']
+#     shares = row['shares']
+#     xprice = row['price']
+#     price = prices[name]
+#     profit = shares*(xprice-price)
+#     print(f'name:{name},profit:{profit:.2f}')
+#     total_profit += profit
+# print('total_profit:',total_profit)
+# total_cost = 0.0
+# cost = 0.0
+# for row in portfolio:
+#     cost = row['shares']*row['price']
+#     total_cost += cost;
+# print(f'total_cost:{total_cost:.2f}')
 
 
 
