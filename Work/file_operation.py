b15dfd9bf40b1bf04b881f03f91b5d710528f688
moduckpig <1828760176@qq.com>
2026-04-10 20:28:35 +0800

    完成file parse2-3代码练习

---
+++ Work/file_operation.py
@@ -0,0 +1,24 @@
+# with open('Data/portfolio.csv','rt') as f:
+#     data = f.read()
+#     print(data)
+# with open('Data/portfolio.csv','rt') as f:
+#     for line in f:
+#         print(line,end='')
+# f = open('Data/portfolio.csv','rt')
+# #next()函数会返回文件的下一行
+# header = next(f)
+# print(header)
+# for line in f:
+#     print(line,end='')
+# f = open('Data/portfolio.csv','rt')
+# headers = next(f).split(',')
+# print (headers)
+# for line in f:
+#     row = line.split(',')
+#     print(row)
+def greeting(name):
+    '输出问候语'
+    print(f'Hello:{name}')
+
+
+
